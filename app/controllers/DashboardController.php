f04def41188ed0a00b386a7333dc209f250aa436
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2023-03-22 23:15:07 +0200

    add header

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -11,9 +11,11 @@ class DashboardController extends Controller {
 		try {
 			$this->view('dashboard/index', [
 				'css'                 => CSS_PATH . 'dashboard.css',
+				'css_header'          => CSS_PATH . 'header.css',
 				'css_project_button'  => CSS_PATH . 'project_button.css',
 				'css_new_project_button'  => CSS_PATH . 'new_project_button.css',
 				'js'                  => JS_PATH  . 'dashboard.js',
+                'js_header'           => JS_PATH  . 'header.js',
 				'js_project_button'   => JS_PATH  . 'project_button.js',
 				'js_new_project_button'   => JS_PATH  . 'new_project_button.js'
 			]);
---
+++ app/views/dashboard/header.php
@@ -0,0 +1,7 @@
+<header class="d-flex position-relative">
+	<button id="hamburger-button" class="rounded position-absolute top-50 translate-middle-y">
+		<img src="<?php echo IMAGES_PATH . "header/hamburger.png" ?>">
+	</button>
+	<img class="position-absolute top-50 start-50 translate-middle" src="<?php echo IMAGES_PATH . "header/logo.png" ?>" alt="">
+	<div></div>
+</header>

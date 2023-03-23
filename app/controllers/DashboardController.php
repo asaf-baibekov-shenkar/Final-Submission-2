7c269b29ce13205313c6230e23cd44b3ebbb4039
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2023-03-23 06:57:54 +0200

    fix indentations

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -10,15 +10,15 @@ class DashboardController extends Controller {
 		}
 		try {
 			$this->view('dashboard/index', [
-				'css'                 => CSS_PATH . 'dashboard.css',
-				'css_header'          => CSS_PATH . 'header.css',
-				'css_sidebar'         => CSS_PATH . 'sidebar.css',
-				'css_project_button'  => CSS_PATH . 'project_button.css',
-				'css_new_project_button'  => CSS_PATH . 'new_project_button.css',
-				'js'                  => JS_PATH  . 'dashboard.js',
-                'js_header'           => JS_PATH  . 'header.js',
-				'js_project_button'   => JS_PATH  . 'project_button.js',
-				'js_new_project_button'   => JS_PATH  . 'new_project_button.js'
+				'css'						=> CSS_PATH . 'dashboard.css',
+				'css_header'				=> CSS_PATH . 'header.css',
+				'css_sidebar'				=> CSS_PATH . 'sidebar.css',
+				'css_project_button'		=> CSS_PATH . 'project_button.css',
+				'css_new_project_button' 	=> CSS_PATH . 'new_project_button.css',
+				'js'						=> JS_PATH  . 'dashboard.js',
+				'js_header'					=> JS_PATH  . 'header.js',
+				'js_project_button'			=> JS_PATH  . 'project_button.js',
+				'js_new_project_button'		=> JS_PATH  . 'new_project_button.js'
 			]);
 		} catch (Illuminate\Database\Eloquent\ModelNotFoundException $exception) {
 			session_destroy();

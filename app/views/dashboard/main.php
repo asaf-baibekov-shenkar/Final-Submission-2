0fedeecb97c78888e171d5c779068ba46d6f49d5
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2023-03-25 01:03:28 +0300

    fix layout and handle responsiveness

--- app/views/dashboard/main.php
+++ app/views/dashboard/main.php
@@ -1,9 +1,9 @@
-<main class="d-flex flex-column">
-	<div class="main-title my-3 text-center">Work</div>
-	<button id="new_task_button" class="rounded mx-5 mb-3 d-flex align-items-center justify-content-center">
+<main class="d-flex flex-column gap-3 pt-3">
+	<div class="main-title text-center">Work</div>
+	<button id="new_task_button" class="rounded mx-5 d-flex align-items-center justify-content-center">
 		<span>New Task</span>
 	</button>
-	<div class="tasks d-flex align-content-start flex-wrap gap-3 h-100 mx-5 pt-3 pb-5 overflow-auto">
+	<div class="tasks d-flex align-content-start flex-wrap gap-2 h-100 mx-5 pb-5 overflow-auto">
 		<?php for ($i = 1; $i <= 10; $i++): ?>
 		<div class="task rounded">
 			<div class="task-title rounded-top d-flex align-items-center justify-content-between">
--- app/views/dashboard/sidebar.php
+++ app/views/dashboard/sidebar.php
@@ -1,5 +1,5 @@
-<div id="sidebar" class="d-flex flex-column align-items-center overflow-auto">
-	<div class="sidebar-title my-3">Projects</div>
+<div id="sidebar" class="d-flex flex-column align-items-center gap-3 pt-3 overflow-auto">
+	<div class="sidebar-title">Projects</div>
 	<?php include 'new_project_button.php' ?>
 	<?php include 'project_button.php' ?>
 </div>

87f081406536450e279ee8f6518f6f84a91fe5f7
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2023-03-25 16:53:45 +0300

    organize public directory

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -10,16 +10,16 @@ class DashboardController extends Controller {
 		// }
 		try {
 			$this->view('dashboard/index', [
-				'css'						=> CSS_PATH . 'dashboard.css',
-				'css_header'				=> CSS_PATH . 'header.css',
-				'css_sidebar'				=> CSS_PATH . 'sidebar.css',
-				'css_main'  				=> CSS_PATH . 'main.css',
-				'css_project_button'		=> CSS_PATH . 'project_button.css',
-				'css_new_project_button' 	=> CSS_PATH . 'new_project_button.css',
-				'js'						=> JS_PATH  . 'dashboard.js',
-				'js_header'					=> JS_PATH  . 'header.js',
-				'js_project_button'			=> JS_PATH  . 'project_button.js',
-				'js_new_project_button'		=> JS_PATH  . 'new_project_button.js'
+				'css'						=> CSS_PATH . 'dashboard/dashboard.css',
+				'css_header'				=> CSS_PATH . 'dashboard/header.css',
+				'css_sidebar'				=> CSS_PATH . 'dashboard/sidebar.css',
+				'css_main'  				=> CSS_PATH . 'dashboard/main.css',
+				'css_project_button'		=> CSS_PATH . 'dashboard/project_button.css',
+				'css_new_project_button' 	=> CSS_PATH . 'dashboard/new_project_button.css',
+				'js'						=> JS_PATH  . 'dashboard/dashboard.js',
+				'js_header'					=> JS_PATH  . 'dashboard/header.js',
+				'js_project_button'			=> JS_PATH  . 'dashboard/project_button.js',
+				'js_new_project_button'		=> JS_PATH  . 'dashboard/new_project_button.js'
 			]);
 		} catch (Illuminate\Database\Eloquent\ModelNotFoundException $exception) {
 			session_destroy();

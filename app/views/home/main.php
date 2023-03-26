7f13febc210dbffc5c808b24ab655b2e0c9b5230
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2023-03-26 09:51:14 +0300

    send post request for login

--- app/views/home/main.php
+++ app/views/home/main.php
@@ -1,16 +1,16 @@
 <main class="position-relative">
-	<form class="login position-absolute top-50 start-50 translate-middle p-3 d-flex flex-column justify-content-between">
+	<form action="<?php echo BASE_URL . 'home/login' ?>" method="POST" class="login position-absolute top-50 start-50 translate-middle p-3 d-flex flex-column justify-content-between">
 		<div class="title d-flex align-items-center justify-content-center rounded">
 			<span>Login</span>
 		</div>
 		<div class="d-flex flex-column gap-3">
 			<div class="form-group">
 				<label for="username" class="d-block p-1 rounded-top">Username</label>
-				<input type="text" class="d-block w-100 border-0 rounded-bottom" id="username">
+				<input name="username" type="text" class="d-block w-100 border-0 rounded-bottom" id="username">
 			</div>
 			<div class="form-group">
 				<label for="password" class="d-block p-1 rounded-top">Password</label>
-				<input type="password" class="d-block w-100 border-0 rounded-bottom" id="password">
+				<input name="password" type="password" class="d-block w-100 border-0 rounded-bottom" id="password">
 			</div>
 		</div>
 		<div class="form-group align-self-center">

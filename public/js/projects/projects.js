431a6cea7180ff6a08fe0ddef22b049fa8bc96af
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2023-04-01 20:46:31 +0300

    redirect to tasks page with the project's id

--- public/js/projects/projects.js
+++ public/js/projects/projects.js
@@ -3,4 +3,10 @@ $(function() {
 	window.projects
 		.map(project => new ProjectCell(project).generateCell())
 		.forEach(cell => { $('#list').append(cell); });
+
+	$('.project .project-description, .project input').on('click', function() {
+		if ($(this).is('input') && $(this).attr('readonly') != 'readonly') return;
+		var id = $(this).closest('.project').attr('project_id');
+		window.location.href = '/public/tasks?id=' + id;
+	});
 });
